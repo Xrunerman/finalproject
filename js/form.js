3e603840bc01f2ffe03f9d81f7cc6fa22f3b9faa
Bohdan Chornyi <wan-xruner@mail.ru>
2017-02-02 23:53:51 +0000

    Initial commit

--- js/form.js
+++ js/form.js
@@ -1,18 +1,30 @@
-$(function() {
-    $('#send').click(function(e) {
-        e.preventDefault();
-        $.ajax({
-            url: "https://formspree.io/wan-xruner@mail.ru",
-            method: "POST",
-            data: {
-                name: $('#name').val(),
-                email: $('#email').val()
-            },
-            dataType: "json"
-        })
-         .done(function(data) {
-             console.dir(data);
-             $('form').html('<h1>Thank you!</h1>');
-         });
+$(document).on('submit', '#contact-form', function(e) {
+    e.preventDefault();
+    var replyto = $('input[name=_replyto]'),
+        replyto_text = replyto.val(),
+        subject = $('input[name=_subject]'),
+        subject_text = subject.val(),
+        body = $('textarea[name=body]'),
+        body_text = body.val();
+
+    $.ajax({
+        url: "https://formspree.io/wan-xruner@mail.ru",
+        method: "POST",
+        data: {
+            _replyto: replyto_text,
+            _subject: subject_text,
+            message: body_text
+        },
+        dataType: "json",
+        beforeSend: function( xhr ) {
+            spinner.show();
+      }
+    }).done(function(){
+        $('#contacts-success-wrapper').show();
+    }).fail(function() {
+        $('#contacts-error-wrapper').show();
+    }).always(function() {
+        spinner.hide();
+        $('#contacts-wrapper').remove();
     });
 });
