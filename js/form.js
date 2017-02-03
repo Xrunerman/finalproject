7e3a56cafecc0f7d20271abc2913b2d4da287b82
Xrunerman <wan-xruner@mail.ru>
2017-02-03 19:28:40 +0200

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,30 +1,29 @@
-$(document).on('submit', '#contact-form', function(e) {
+function sendForm() {
+  var HIDDEN_CLASS = 'hidden';
+  var message = document.getElementById('message');
+  var success = document.querySelector('.success');
+  var fail = document.querySelector('.fail');
+  var form = document.forms[0];
+  var btn = document.getElementById('submit');
+  var request = new XMLHttpRequest();
+  request.open('POST', '//formspree.io/wan-xruner@mail.ru', true);
+  request.setRequestHeader('accept', 'application/json');
+  btn.addEventListener('click', function(e) {
     e.preventDefault();
-    var replyto = $('input[name=_replyto]'),
-        replyto_text = replyto.val(),
-        subject = $('input[name=_subject]'),
-        subject_text = subject.val(),
-        body = $('textarea[name=body]'),
-        body_text = body.val();
-
-    $.ajax({
-        url: "https://formspree.io/wan-xruner@mail.ru",
-        method: "POST",
-        data: {
-            _replyto: replyto_text,
-            _subject: subject_text,
-            message: body_text
-        },
-        dataType: "json",
-        beforeSend: function( xhr ) {
-            spinner.show();
+    var formData = new FormData(form);
+    request.send(formData);
+    request.onreadystatechange = function() {
+      form.classList.add(HIDDEN_CLASS);
+      message.classList.remove(HIDDEN_CLASS);
+      if (request.readyState === 4 && request.status === 200) {
+        success.classList.remove(HIDDEN_CLASS);
+        fail.classList.add(HIDDEN_CLASS);
       }
-    }).done(function(){
-        $('#contacts-success-wrapper').show();
-    }).fail(function() {
-        $('#contacts-error-wrapper').show();
-    }).always(function() {
-        spinner.hide();
-        $('#contacts-wrapper').remove();
-    });
-});
+      else {
+        success.classList.add(HIDDEN_CLASS);
+        fail.classList.remove(HIDDEN_CLASS);
+      }
+    }
+  });
+}
+sendForm();
